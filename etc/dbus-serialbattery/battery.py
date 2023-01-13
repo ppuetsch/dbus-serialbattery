973afef2473b6d7a1fb454f18d9dd5f4df9b99bd
Philip Puetsch <philip.puetsch@d-fine.com>
2023-01-13 20:36:53 +0100

    remove development logging

--- etc/dbus-serialbattery/battery.py
+++ etc/dbus-serialbattery/battery.py
@@ -609,20 +609,11 @@ class Battery(ABC):
         return
 
     def calcDynamicDischargeCurrent(self):
-        logger.info(
-            f"calculating dynamic discharge limit min_cell_voltage = "
-            f"{self.get_min_cell_voltage()} current = {self.current} "
-            f"internal_resistance = {utils.INTERNAL_BATTERY_RESISTANCE}"
-        )
         estimated_zero_load_voltage = (
             self.get_min_cell_voltage()
             - self.current * utils.INTERNAL_BATTERY_RESISTANCE
         )
-        logger.info(f" estimated_zero_load_voltage = {estimated_zero_load_voltage}")
         available_voltage_drop = estimated_zero_load_voltage - utils.MIN_CELL_VOLTAGE
-        logger.info(f"available_voltage_drop = {available_voltage_drop}")
         available_current = available_voltage_drop / utils.INTERNAL_BATTERY_RESISTANCE
-        logger.info(f"available_current = {available_current}")
         result = max(available_current, 5)
-        logger.info(f"result = {result}")
         return result
